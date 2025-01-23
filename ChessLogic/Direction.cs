857ad6b9090a84ab130aa6b29ad276aae3f4836d
Matthew Meadows <69929333+Matt9907@users.noreply.github.com>
2025-01-23 14:32:02 -0500

    Assigned values to cardinal moves for pieces

--- ChessLogic/Direction.cs
+++ ChessLogic/Direction.cs
@@ -15,7 +15,27 @@ namespace ChessLogic
         public readonly static Direction NorthWest = North + West;
         public readonly static Direction NorthEast = North + East;
         public readonly static Direction SouthWest = South + West;
-        public readonly static Direction SouthEast = South + East; 
+        public readonly static Direction SouthEast = South + East;
 
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction a, Direction b)
+        {
+            return new Direction(a.RowDelta + b.RowDelta, a.ColumnDelta + b.ColumnDelta);
+        }
+
+        public static Direction operator *(int scalar, Direction dir)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+
+        }
     }
 }
