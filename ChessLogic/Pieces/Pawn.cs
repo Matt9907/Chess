1af52bb342bf3ecab790e5b953ad393564946c7d
Matthew Meadows <69929333+Matt9907@users.noreply.github.com>
2025-01-23 14:45:45 -0500

    Created Pawn class, and abstracted from Pieces

---
+++ ChessLogic/Pieces/Pawn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Pawn : Pieces
+    {
+        public override PieceType Type => PieceType.Pawn;
+        public override Player Color { get; }
+        public Pawn(Player color)
+        {
+            Color = color;
+        }
+
+        public override Pieces Copy()
+        {
+            Pawn copy = new Pawn(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
