5a926baf9f9061cc33b10fff966a9a7c6e958f33
Matthew Meadows <69929333+Matt9907@users.noreply.github.com>
2025-01-23 14:41:00 -0500

    Abstract class for Pieces to pull from

---
+++ ChessLogic/Pieces/Pieces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public abstract class Pieces
+    {
+        public abstract PieceType Type { get; }
+        public abstract Player Color { get; }
+
+        public bool HasMoved { get; set; } = false;
+
+        public abstract Pieces Copy();
+    }
+}
