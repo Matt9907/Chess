f8d66d8c58d6ced15f850623ae71dc02bedf9055
Matthew Meadows <69929333+Matt9907@users.noreply.github.com>
2025-01-23 14:06:56 -0500

    Fixed position class for determining what color square user starts on.

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    class Direction
+    {
+        public readonly static Direction North = new Direction(-1, 0);
+        public readonly static Direction South = new Direction(1, 0);
+        public readonly static Direction East = new Direction(0, 1);
+        public readonly static Direction West = new Direction(0, -1);
+        public readonly static Direction NorthWest = North + West;
+        public readonly static Direction NorthEast = North + East;
+        public readonly static Direction SouthWest = South + West;
+        public readonly static Direction SouthEast = South + East; 
+
+    }
+}
